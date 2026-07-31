8452f1c0e1d8f6654d3956c1fffb1420b43edff9
Morgan Favour <morganfavour85@gmail.com>
2026-07-31 02:13:30 +0100

    feat: add WorkerInviteModal component for creating leader invites

---
+++ src/app/invite/worker/[token]/page.tsx
@@ -0,0 +1,8 @@
+export default function InviteWorkerPage({
+  params,
+}: {
+  params: Promise<{ token: string }> | { token: string };
+}) {
+  // your page content
+  return <div>Invite worker page</div>;
+}
